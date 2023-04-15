aa55c820a7a06a9b7e4df1ec39a00b10cc01c935
Sharone Nixon <shanix@live.de>
2023-04-15 20:22:15 +0200

    added dynamic lessons hamburger

--- src/components/LessonsBurgerMenu/LessonsBurgerMenu.tsx
+++ src/components/LessonsBurgerMenu/LessonsBurgerMenu.tsx
@@ -4,16 +4,13 @@ import Link from "next/link"
 import { usePathname } from 'next/navigation';
 import { ethLessons, btcLessons, dotLessons, Lesson } from "../../data/lessonsData";
 
-type Props = {}
 
 export default function LessonsBurgerMenu({ lessons }: any) {
 
-  const [openLessons, setOpenLessons] = useState(false);
+  const [openLessons, setOpenLessons] = useState<boolean>(false);
   const [currentLessons, setCurrentLessons] = useState<Lesson[]>();
+  const [currentSection, setCurrentSection] = useState<string>("");
   const pathname = usePathname();
-  // const currentLessons:any = [];
-  console.log(currentLessons)
-  console.log(pathname.startsWith("/eth"))
 
   const handleLessonsMenuClick = () => {
     setOpenLessons(!openLessons)
@@ -21,31 +18,34 @@ export default function LessonsBurgerMenu({ lessons }: any) {
   useEffect(() => {
     if (pathname.startsWith('/eth')) {
       setCurrentLessons(ethLessons);
+      setCurrentSection("eth")
     } else if (pathname.startsWith('/btc')) {
       setCurrentLessons(btcLessons);
+      setCurrentSection("btc")
     } else if (pathname.startsWith('/dot')) {
       setCurrentLessons(dotLessons);
+      setCurrentSection("dot")
     }
   }, [])
+
+
 
   return (
     <div className="absolute left-0 top-28 flex ">
       <div className={` ${!openLessons && "hidden"} bg-bgDarkGray flex flex-col p-5 rounded-br-lg `} >
-        <Link href={"/"} >Dis is Link</Link>
-        <Link href={"/"} >Dis is Link</Link>
-        <Link href={"/"} >Dis is Link</Link>
-        <Link href={"/"} >Dis is Link</Link>
-        <Link href={"/"} >Dis is Link</Link>
-        <Link href={"/"} >Dis is Link</Link>
+        {currentLessons && currentLessons.map(lesson => (
+          <Link href={`/${currentSection}/${lesson.slug}`} key={lesson.id} >{lesson.title}</Link>
+        ))
+        }
       </div>
       <button
-        className=" bg-bgDarkGray w-16 h-14 rounded-tr-lg rounded-br-lg flex flex-col items-center justify-evenly py-1.5 "
+        className=" bg-bgDarkGray w-12 h-12 rounded-tr-lg rounded-br-lg flex flex-col items-center justify-evenly py-1.5 "
         onClick={handleLessonsMenuClick}
       >
 
-        <div className="bg-primaryWhite h-1.5 w-10 rounded-md "></div>
-        <div className="bg-primaryWhite h-1.5 w-10 rounded-md "></div>
-        <div className="bg-primaryWhite h-1.5 w-10 rounded-md "></div>
+        <div className="bg-primaryWhite h-1 w-7 rounded-md "></div>
+        <div className="bg-primaryWhite h-1 w-7 rounded-md "></div>
+        <div className="bg-primaryWhite h-1 w-7 rounded-md "></div>
       </button>
     </div>
   )
