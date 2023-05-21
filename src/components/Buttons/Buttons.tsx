09c1183b6482641c6573d7acba588a2b6b7c7d86
Sharone Nixon <shanix@live.de>
2023-05-21 12:43:00 +0200

    :sparkles: added newsletter and podcast section on landing page

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -6,32 +6,48 @@ type Props = {
   children: React.ReactNode,
   onClick?: Function,
   href?: string,
+  target?: "_blank",
 }
 
 
-export function PrimaryButton({ children, onClick, href }: Props) {
+export function PrimaryButton({ children, onClick, href, target }: Props) {
   return (
-    <div className="bg-gradient-to-r from-primaryBlue via-secondaryPurple to-primaryPink px-7 py-2 rounded font-bold text-lg" >
+    <div className="bg-gradient-to-r from-primaryBlue via-secondaryPurple to-primaryPink px-7 py-2 rounded font-bold text-lg text-white" >
       <Link
         href={href ? href : "/"}
         onClick={() => onClick?.()}
+        target={target ? target : "_self"}
       >
         {children}
       </Link>
     </div>
   )
 }
-export function SecondaryButton({ children, onClick, href }: Props) {
+export function SecondaryButton({ children, onClick, href, target }: Props) {
   return (
     <div className="bg-gradient-to-r from-primaryBlue via-secondaryPurple to-primaryPink p-1  rounded flex">
       <Link
         href={href ? href : "/"}
         className="bg-white dark:bg-bgDarkBlue px-7 py-1 w-full h-full rounded font-bold text-lg "
         onClick={() => onClick?.()}
+        target={target ? target : "_self"}
       >
-        {/* <div > */}
         {children}
-        {/* </div> */}
+      </Link>
+    </div>
+  )
+}
+
+export function GeneralButton({ children, onClick, href, target }: Props) {
+  return (
+    <div className=" bg-slate-200 dark:bg-bgDarkGray p-1  rounded flex">
+      <Link
+        href={href ? href : "/"}
+        className=" px-7 py-1 w-full h-full  font-bold text-lg "
+        onClick={() => onClick?.()}
+        target={target ? target : "_self"}
+      >
+        {children}
       </Link>
     </div>
   )
