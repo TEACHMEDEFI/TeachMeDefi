207694ee9f3bc63d2f581dda53a799d0d7213fe8
Sharone Nixon <shanix@live.de>
2023-06-08 12:14:00 +0200

    added eth quest transcript :speech_balloon:

--- src/data/eth/index.ts
+++ src/data/eth/index.ts
@@ -1,10 +1,21 @@
 
 import { quest1 } from "./quest-1";
 import { quest2 } from "./quest-2";
+import { quest3 } from "./quest-3";
+import { quest4 } from "./quest-4";
+import { quest5 } from "./quest-5";
+import { quest6 } from "./quest-6";
+import { questSicherheit } from "./quest-sicherheit";
+
 
 export const ethQuests = [
   ...quest1,
   ...quest2,
+  ...quest3,
+  ...quest4,
+  ...quest5,
+  ...quest6,
+  ...questSicherheit,
 ]
 
 
