4a8642c8a1e739046dfe5f425d9f9efd917ee7b1
Patrick <>
2023-07-04 10:31:30 +0200

    stickyHelper

--- src/components/Buttons/StickyHelpButton.tsx
+++ src/components/Buttons/StickyHelpButton.tsx
@@ -18,9 +18,9 @@ export default function StickyHelpButton() {
   return (
     <div className="fixed right-5 lg:right-16 bottom-0 flex flex-col z-50">
       <div
-        className={`bg-gray-300 dark:bg-bgDarkGray h-12 flex items-center justify-evenly rounded-t-md  `}
+        className="bg-gray-300 dark:bg-bgDarkGray h-12 flex items-center justify-evenly rounded-t-md"
       >
-        <button className={`h-full flex items-center rounded-t-md px-2 lg:px-5 font-bold`}
+        <button className="h-full flex items-center rounded-t-md px-2 lg:px-5 bg-gray-200 dark:bg-gray-600"
           onClick={scrollToElement} >
           <span className="h-6 w-6 relative  lg:mr-2">
             <Image src={isDarkMode ? "/cookie-light.svg" : "/cookie-dark.svg"}
