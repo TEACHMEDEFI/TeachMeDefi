598d325712e46b5c667c1a159e354a889e119837
Patrick <>
2023-07-04 10:21:39 +0200

    Spinner

--- src/components/Modals/QuestClaimModalDot.tsx
+++ src/components/Modals/QuestClaimModalDot.tsx
@@ -148,12 +148,7 @@ const QuestClaimModalDot = ({questSectionId, togglePopup, setSelectedPolkaAccoun
                 {showSpinner ? (
                     <>
                         <p>Dein NFT Ist Auf Dem Weg!</p>
-                            <Spinner
-                            thickness='4px'
-                            speed='0.65s'
-                            emptyColor='gray.200'
-                            color='blue.500'
-                            size='xl' /> 
+                            <Spinner /> 
                     </>
                     ) : null
                 }
