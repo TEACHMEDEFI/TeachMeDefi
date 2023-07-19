e0615877ae2f8d7b8d373037854e4b602e9a3ab3
Patrick <>
2023-07-19 08:05:43 +0200

    event listener for outside click

--- src/components/Modals/VideoModal.tsx
+++ src/components/Modals/VideoModal.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import { useEffect } from 'react';
 import { Lesson } from "@/data/generalLessons";
 import LessonsBurgerMenu from "@/components/LessonsBurgerMenu/LessonsBurgerMenu";
 import VideoWithTranscript from "@/components/VideoWithTranscript/VideoWithTranscript";
@@ -20,15 +21,34 @@ export default function LessonPage({ currentLesson, modalOpen, onClose, setUserP
 
     const handleClose = () => {
         onClose(currentLesson, questSectionid)
-      }
+    }
+
+      const handleOutsideClick = (event: MouseEvent) => {
+        const modalElement = document.querySelector('.lesson-page-modal');
+        if (modalElement && !modalElement.contains(event.target as Node)) {
+            console.log('Clicked')
+            handleClose();
+        }
+    };
+
+    useEffect(() => {
+        if (modalOpen) {
+            document.addEventListener('mousedown', handleOutsideClick);
+            console.log('click listener added')
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, [modalOpen]);
 
     if (!modalOpen) {
         return (<></>)
     }
 
     return (
-        <div className='fixed backdrop-blur-md top-0 w-screen h-screen left-0 z-50 flex items-center justify-center ' >
-            <div className='relative w-[80rem] bg-gray-300 dark:bg-bgDarkerGray rounded-lg flex flex-col justify-center gap-5 px-8 py-16' >
+        <div className='fixed backdrop-blur-md top-0 w-screen h-screen left-0 z-50 flex items-center justify-center' >
+            <div className='relative w-[80rem] bg-gray-300 dark:bg-bgDarkerGray rounded-lg flex flex-col justify-center gap-5 px-8 py-16 lesson-page-modal' >
                 <VideoWithTranscript currentLesson={currentLesson} setUserProgress={setUserProgress}  />
                 <LessonsBurgerMenu />
                 {/* <LinksAndIndexBurger /> */}
