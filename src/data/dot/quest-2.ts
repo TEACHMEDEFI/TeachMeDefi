d321b91b403bcc21f9b07699ef8b65072b9d17cc
moeeinvest <139962195+moeeinvest@users.noreply.github.com>
2023-07-19 17:16:43 +0200

    Update quest-2.ts
    nummerierung

--- src/data/dot/quest-2.ts
+++ src/data/dot/quest-2.ts
@@ -10,7 +10,7 @@ import { Lesson } from "../generalLessons";
 export const quest2: Lesson[] = [
   {
     id: "dot-11",
-    title: '2.1 DOT kaufen',
+    title: 'Quest 2.1 DOT kaufen',
     slug: 'dot-kaufen',
     youtubeUrl: 'https://youtu.be/2PsLjRfAJbQ',
     youtubeThumbnail: "https://youtu.be/2PsLjRfAJbQ/hqdefault.jpg",
@@ -94,7 +94,7 @@ export const quest2: Lesson[] = [
   },
   {
     id: "dot-12",
-    title: '2.2 DOT senden',
+    title: 'Quest 2.2 DOT senden',
     slug: 'dot-senden',
     youtubeUrl: 'https://youtu.be/JggH_V_bsfQ',
     youtubeThumbnail: "https://youtu.be/JggH_V_bsfQ/hqdefault.jpg",
@@ -197,7 +197,7 @@ export const quest2: Lesson[] = [
   },
   {
     id: "dot-13",
-    title: '2.3 Block Explorer nutzen',
+    title: 'Quest 2.3 Block Explorer nutzen',
     slug: 'block-explorer-nutzen',
     youtubeUrl: 'https://youtu.be/xn3c4k1YdB8',
     youtubeThumbnail: "https://youtu.be/xn3c4k1YdB8/hqdefault.jpg",
