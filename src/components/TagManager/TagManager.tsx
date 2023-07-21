54e658858527c92bc43a1bc25c658533122b74d9
Patrick <>
2023-07-21 07:54:54 +0200

    prepare tagmanager stop if cookies are not accepted

--- src/components/TagManager/TagManager.tsx
+++ src/components/TagManager/TagManager.tsx
@@ -1,9 +1,35 @@
 import Script from "next/script";
 import type { NextPage } from "next";
+import { useState, useEffect } from 'react';
 
 const GTM_ID = process.env.NEXT_PUBLIC_TAGMANAGER_CONTAINER_ID;
 
 const TagManagerScript: NextPage = () => {
+  const [acceptedCookies, setAcceptedCookies] = useState<boolean>(false)
+
+
+  useEffect(() => {
+    console.log('Setting Tag Manager')
+    if (typeof window !== 'undefined') {
+        
+        const cookieBannerInteraction = localStorage.getItem('cookieBannerInteraction');
+        console.log(cookieBannerInteraction)
+        if (cookieBannerInteraction) {
+            const acceptedCookies = JSON.parse(cookieBannerInteraction);
+            const allCookiesAccepted = acceptedCookies && acceptedCookies.cookies === 'all';
+            console.log('Set cookie accepted', allCookiesAccepted)
+            setAcceptedCookies(allCookiesAccepted)
+
+        }
+    }
+  }, []);
+
+
+  // if (!acceptedCookies) {
+  //   return (<></>)
+  // }
+
+
   return (
     <Script id="google-tag-manager" strategy="afterInteractive">
       {`
@@ -19,6 +45,24 @@ const TagManagerScript: NextPage = () => {
 
 const TagManagerNoScript: NextPage = () => {
   const tagManagerUrl = `https://www.googletagmanager.com/ns.html?id=${GTM_ID}`;
+  const [acceptedCookies, setAcceptedCookies] = useState<boolean>(false)
+
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+        const cookieBannerInteraction = localStorage.getItem('cookieBannerInteraction');
+        if (cookieBannerInteraction) {
+            const acceptedCookies = JSON.parse(cookieBannerInteraction);
+            const allCookiesAccepted = acceptedCookies && acceptedCookies.cookies === 'all';
+            setAcceptedCookies(allCookiesAccepted)
+        }
+    }
+  }, []);
+
+
+  // if (!acceptedCookies) {
+  //   return (<></>)
+  // }
 
   return (
     <noscript id="google-tag-manager-no-script">
