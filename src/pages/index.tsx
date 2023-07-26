4bdf2adaa61c086fbe5ef06ed9762c76feb54827
Sharone Nixon <shanix@live.de>
2023-07-26 20:30:19 +0200

    feat: added custom preview link with og meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,11 @@ export default function Home() {
         <meta name="description" content="TeachMeDefi teach me defi" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/TDM_logo.png" />
+        <meta property="og:title" content="TEACHMEDEFI" />
+        <meta property="og:description" content="Krypto leicht gemacht" />
+        <meta property="og:image" content="https://mario.wiki.gallery/images/thumb/8/81/NSMBU_Bubble_Baby_Yoshi_Artwork.png/1200px-NSMBU_Bubble_Baby_Yoshi_Artwork.png" />
+        <meta property="og:url" content="https://tmd-dev.netlify.app" />
+        <meta property="og:type" content="website" />
       </Head>
 
       <HeroSection />
