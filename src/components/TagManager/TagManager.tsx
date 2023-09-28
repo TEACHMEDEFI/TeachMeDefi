610d0f8c1c650c7aa8a9b44d1eea25cf37305df6
Patrick <>
2023-09-28 14:20:35 +0200

    GTM consent mode

--- src/components/TagManager/TagManager.tsx
+++ src/components/TagManager/TagManager.tsx
@@ -25,33 +25,37 @@ const useAcceptedCookies = () => {
 const TagManagerScript: NextPage = () => {
   const acceptedCookies = useAcceptedCookies();
 
-
-  if (!acceptedCookies) {
-    return (<></>)
-  }
-
-
   return (
-    <Script id="google-tag-manager" strategy="afterInteractive">
-      {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-      `}
-    </Script>
+    <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+      <Script id="google-tag-manager-consent" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          window.dataLayer.push({
+            'event': 'consent_declaration',
+            'consentSettings': {
+              'ad_storage': '${acceptedCookies ? 'granted' : 'denied'}',
+              'analytics_storage': '${acceptedCookies ? 'granted' : 'denied'}',
+              // add other consent settings if needed
+            }
+          });
+        `}
+      </Script>
+    </>
   );
 };
 
 const TagManagerNoScript: NextPage = () => {
-  const acceptedCookies = useAcceptedCookies();
   const tagManagerUrl = `https://www.googletagmanager.com/ns.html?id=${GTM_ID}`;
 
-
-  if (!acceptedCookies) {
-    return (<></>)
-  }
 
   return (
     <noscript id="google-tag-manager-no-script">
