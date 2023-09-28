fdfe28cdaa444e719e274880c618d613d720280c
Patrick <>
2023-09-28 14:34:55 +0200

    GTM Consent mode small fix

--- src/components/TagManager/TagManager.tsx
+++ src/components/TagManager/TagManager.tsx
@@ -25,31 +25,30 @@ const useAcceptedCookies = () => {
 const TagManagerScript: NextPage = () => {
   const acceptedCookies = useAcceptedCookies();
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        'event': 'consent_declaration',
+        'consentSettings': {
+          'ad_storage': acceptedCookies ? 'granted' : 'denied',
+          'analytics_storage': acceptedCookies ? 'granted' : 'denied',
+          // add other consent settings if needed
+        }
+      });
+    }
+  }, [acceptedCookies]);  // This ensures the effect runs whenever acceptedCookies changes
+
   return (
-    <>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-      </Script>
-      <Script id="google-tag-manager-consent" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          window.dataLayer.push({
-            'event': 'consent_declaration',
-            'consentSettings': {
-              'ad_storage': '${acceptedCookies ? 'granted' : 'denied'}',
-              'analytics_storage': '${acceptedCookies ? 'granted' : 'denied'}',
-              // add other consent settings if needed
-            }
-          });
-        `}
-      </Script>
-    </>
+    <Script id="google-tag-manager" strategy="afterInteractive">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      `}
+    </Script>
   );
 };
 
