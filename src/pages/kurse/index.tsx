4478c0853845ead289a1e11ac0790c5bd8c917df
Sharone Nixon <shanix@live.de>
2023-12-10 12:42:10 +0100

    chore: :lipstick: adjusted rgb donut on courses landing page

--- src/pages/kurse/index.tsx
+++ src/pages/kurse/index.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 export default function index() {
   return (
-    <main className='py-12 flex flex-col-reverse lg:flex-row max-lg:justify-end 
-    min-h-[80vh] xl:w-[1240px] mx-auto px-5'>
+    <main className='py-12 flex flex-col lg:flex-row max-lg:justify-end 
+    lg:min-h-[700px] xl:w-[1240px] mx-auto px-5'>
 
-      <div className='w-1/2'>
+      <div className='text-center lg:text-start lg:w-1/2 lg:pt-32 xl:pt-24'>
 
         <h1 className='xl:text-5xl text-4xl font-bold mb-5' >
           ÜBERBLICK ÜBER UNSERE KURSE
@@ -16,7 +16,7 @@ export default function index() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quidem cupiditate, adipisci sit fugiat voluptate architecto reprehenderit voluptas quia sunt sed voluptates enim dolorem modi. Id dolore ipsa pariatur inventore.
           Dolor accusamus aliquam blanditiis, placeat voluptatum sapiente veritatis, eos quam perspiciatis inventore dolores eveniet rerum commodi, quidem unde ut omnis nulla id? Ut nam ullam, laudantium voluptatibus dicta commodi esse.
         </p>
-        <div className='flex flex-wrap gap-4 '>
+        <div className='flex flex-wrap gap-4 max-lg:justify-center'>
           <PrimaryButton customClassWrapper='max-sm:w-44 text-center' href='/eth' >
             ETHEREUM
           </PrimaryButton>
@@ -31,25 +31,16 @@ export default function index() {
 
 
       <div className='relative flex-1'>
-        {/* <div className='courses-donut' /> */}
-
-        <div className="absolute left-0 translate-x-[-10%] top-0 translate-y-[-10%] w-[250%] aspect-square">
-          <Image src={"/home/landing_rgb_donut.svg"} fill alt="_________" />
+        <div className=" absolute  md:hidden w-[105vw] min-w-[700px] left-1/2 translate-x-[-50%] aspect-[256/125]">
+          <Image src={"/home/landing-stripe-sm.svg"} fill alt="Gradient background with Bitcoin, Ethereum, Polkadot, Brave, Talisman, Metamask, Trezor, Ledger and Trust Wallet logos" />
         </div>
-        {/* <div className="absolute left-[50%] translate-x-[-50%] top-0 w-[80%] aspect-[707/390]">
-          <Image src={"/kurse-page/old-ship-no-trust.png"} fill alt="_________" />
+        <div className=" absolute max-md:hidden lg:hidden w-[105vw] min-w-[900px] left-1/2 translate-x-[-50%] aspect-[256/125]">
+          <Image src={"/home/landing-stripe.svg"} fill alt="Gradient background with Bitcoin, Ethereum, Polkadot, Brave, Talisman, Metamask, Trezor, Ledger and Trust Wallet logos" />
         </div>
-        <div className="absolute left-[46%] translate-x-[-50%] top-20 w-[50%] aspect-[337/435] ">
-          <Image src={"/kurse-page/merge-ship.png"} fill alt="_________" />
-        </div> */}
-        {/* <div className="relative w-full sm:w-[500px] lg:w-full aspect-[780/533] ">
-          <Image src={"/kurse-page/kurse-hero-people.png"} fill alt="_________" />
-        </div>  */}
-
-
-
-
-
+        <div className="absolute hidden lg:block left-0 translate-x-[-10%] top-0 translate-y-[-10%] w-[250%] aspect-square">
+          <Image src={"/home/landing_rgb_donut.svg"} fill alt="Gradient background with Bitcoin, Ethereum, Polkadot, Brave, Talisman, Metamask, Trezor, Ledger and Trust Wallet logos" />
+        </div>
+        <div className='h-[300px] md:h-[400px] lg:hidden' />
 
       </div>
 
